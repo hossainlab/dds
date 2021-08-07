3fa5b47306c424738f648c1130e8bb6bac85f18c
Jubayer <hellojubayer.hossain@gmail.com>
2021-08-08 01:07:47 +0600

    reqs

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = MultiApp()
 
 st.markdown("""
 # CHIRAL Diseases Detection System (CHIRAL-DDS)
- By [Md. Jubayer Hossain]()
+ By [Md. Jubayer Hossain](https://jhossain.netlify.app/)
  
 This project aims to help doctors or medical professionals make data-driven decisions in health care, and they can easily detect patients with the disease. 
 """)
